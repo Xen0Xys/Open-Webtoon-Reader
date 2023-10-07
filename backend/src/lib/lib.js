86ef350585a247dc22105288f7d962343bc38c2c
Czekaj Tom <red57101@gmail.com>
2023-10-07 18:57:08 +0200

    :sparkles: Add lib.js to manage webtoon caching

---
+++ backend/src/lib/lib.js
@@ -0,0 +1,59 @@
+const {getWebtoons, findWebtoon} = require("./utils/webtoon");
+const {saveInDatabase} = require("./utils/saving/databaseSaving");
+
+let webtoons = {}
+const languages = [
+    "fr",
+    "en",
+    "es"
+]
+let cacheLoaded = false;
+let downloadStarted = false;
+
+async function loadWebtoonCache(){
+    webtoons = {};
+    cacheLoaded = false;
+    console.log("Loading webtoon cache");
+    for(const language of languages){
+        webtoons[language] = await getWebtoons(language);
+        console.log("Webtoons loaded for language " + language);
+    }
+    cacheLoaded = true;
+}
+
+function getWebtoonsCache(){
+    return webtoons;
+}
+
+function isCacheLoaded(){
+    return cacheLoaded;
+}
+
+function isDownloading(){
+    return downloadStarted;
+}
+
+function findWebtoonInCache(webtoonName, language){
+    return findWebtoon(webtoons[language], webtoonName);
+}
+
+function startDownload(webtoon, startEpisode){
+    downloadStarted = true;
+    saveInDatabase(webtoon, startEpisode).then(() => {
+        downloadStarted = false;
+    }).catch((err) => {
+        console.error(err);
+        downloadStarted = false;
+    });
+}
+
+loadWebtoonCache().then(() => {
+    console.log("Webtoon cache loaded");
+});
+
+module.exports = {
+    loadWebtoonCache,
+    getWebtoonsCache,
+    isCacheLoaded,
+    isDownloading,
+}
