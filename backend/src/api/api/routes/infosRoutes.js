1d1762d00597cb6b3981c66cd9dce227686ac378
Czekaj Tom <red57101@gmail.com>
2023-10-07 18:58:48 +0200

    :rocket: Update webtoon information routes

--- backend/src/api/api/routes/infosRoutes.js
+++ backend/src/api/api/routes/infosRoutes.js
@@ -1,13 +1,14 @@
 const {getWebtoons, getWebtoonEpisodes, getEpisodeImages} = require("../controllers/webtoonControllers");
+const authentication = require("../middlewares/authentication");
 
 module.exports = (router) => {
-    router.get("/webtoons", async (req, res) => {
+    router.get("/webtoons", authentication, async (req, res) => {
         await getWebtoons(req, res);
     });
-    router.get("/webtoons/:webtoon_id/episodes", async (req, res) => {
+    router.get("/webtoons/:webtoon_id/episodes", authentication, async (req, res) => {
         await getWebtoonEpisodes(req, res);
     });
-    router.get("/webtoons/:webtoon_id/episode/:episode_number", async (req, res) => {
+    router.get("/webtoons/:webtoon_id/episode/:episode_number", authentication, async (req, res) => {
         await getEpisodeImages(req, res);
     });
 };
