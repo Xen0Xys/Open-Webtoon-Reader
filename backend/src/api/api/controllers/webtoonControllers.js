bc72a5da8a0e2a5c2b183184ef2a2f0b4f19650c
Czekaj Tom <red57101@gmail.com>
2023-10-09 11:07:21 +0200

    :ambulance: Fix api crash

--- backend/src/api/api/controllers/webtoonControllers.js
+++ backend/src/api/api/controllers/webtoonControllers.js
@@ -1,7 +1,9 @@
 const sequelize = require("../../../common/database/sequelize");
 
 async function getWebtoons(req, res) {
-    const webtoons = await sequelize.models.webtoons.findAll({attributes: ["id", "title", "author", "language", "genre_id", "thumbnail"]});
+    const webtoons = await sequelize.models.webtoons.findAll({attributes: ["id", "title", "author", "language", "genre_id", "thumbnail"], order: [["id", "DESC"]]});
+    if(!webtoons.length)
+        return res.status(404).json({message: "Webtoons not found"});
     const jsonWebtoons = webtoons.map((webtoon) => webtoon.toJSON());
     for (const webtoon of jsonWebtoons)
         webtoon.episode_count = await sequelize.models.episodes.count({where: {webtoon_id: webtoon.id}});
@@ -11,7 +13,11 @@ async function getWebtoons(req, res) {
 async function getWebtoonEpisodes(req, res){
     const webtoonId = req.params.webtoon_id;
     const webtoon = await sequelize.models.webtoons.findOne({where: {id: webtoonId}});
+    if(!webtoon)
+        return res.status(404).json({message: "Webtoon not found"});
     const episodes = await sequelize.models.episodes.findAll({where: {webtoon_id: webtoonId}});
+    if(!episodes.length)
+        return res.status(404).json({message: "Episodes not found"});
     return res.status(200).json({
         background_banner: webtoon.background_banner,
         top_banner: webtoon.top_banner,
@@ -23,8 +29,12 @@ async function getWebtoonEpisodes(req, res){
 async function getEpisodeImages(req, res){
     const webtoonId = req.params.webtoon_id;
     const episodeNumber = req.params.episode_number;
-    const episodeId = await sequelize.models.episodes.findOne({where: {webtoon_id: webtoonId, number: episodeNumber}});
-    const images = await sequelize.models.images.findAll({where: {episode_id: episodeId.id}, order: [["number", "ASC"]], attributes: ["image"]});
+    const episode = await sequelize.models.episodes.findOne({where: {webtoon_id: webtoonId, number: episodeNumber}});
+    if(!episode)
+        return res.status(404).json({message: "Episode not found"});
+    const images = await sequelize.models.images.findAll({where: {episode_id: episode.id}, order: [["number", "ASC"]], attributes: ["image"]});
+    if(!images.length)
+        return res.status(404).json({message: "Images not found"});
     res.status(200).json(images);
 }
 
