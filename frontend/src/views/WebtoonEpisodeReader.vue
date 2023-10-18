fb9bcf58a39594399d152da580193e651abea269
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-10-18 13:11:40 +0200

    :ambulance: Fix reader redirection on reload

--- frontend/src/views/WebtoonEpisodeReader.vue
+++ frontend/src/views/WebtoonEpisodeReader.vue
@@ -4,24 +4,22 @@ import axios from 'axios'
 export default {
   name: 'WebtoonEpisodeReader',
   data: () => ({
-    webtoon: undefined
+    images: []
   }),
   created () {
     const webtoonId = parseInt(this.$route.params.webtoon)
     const episodeId = parseInt(this.$route.params.episode)
-    this.webtoon = this.$store.state.webtoons.filter(w => w.id === webtoonId)[0]
-    if (!this.webtoon) return this.$router.push('/library')
 
     axios.get(`/webtoons/${webtoonId}/episode/${episodeId}`).then(({ data }) => {
-      this.webtoon.images = data.map(i => i.image)
+      this.images = data.map(i => i.image)
     }).catch(console.log)
   }
 }
 </script>
 
 <template>
-  <div class="episode" v-if="webtoon">
-    <img :src="image" alt="" class="episode--image" :key="`img-${image}`" v-for="image in webtoon.images" />
+  <div class="episode" v-if="images.length > 0">
+    <img :src="image" alt="" class="episode--image" :key="`img-${image}`" v-for="image in images" />
   </div>
 </template>
 
